c527b3829c7ff1628bdb45dcbbaa9fbd0e1a12e8
mastermindshudipta <sayshudipta@gmail.com>
2025-04-15 13:54:22 +0600

    learning recursion

---
+++ python/recursion.py
@@ -0,0 +1,19 @@
+def print_num(n: int):
+    # base condition
+    if n == 0:
+        return
+    
+    # recurse
+    print_num(n-1)
+    # after previous function call
+    print(n)
+    
+
+
+def main():
+    print_num(3)
+
+
+if __name__ == "__main__":
+    main()
+    print("Done")
